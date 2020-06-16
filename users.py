3d1086dc34935c8c432036d80d2d495f3cfbd27a
Emmanuel Lucio Urbina <emmanuelluur@outlook.com>
2020-06-15 23:33:42 -0500

    + ordenamiento por edad descendente

--- users.py
+++ users.py
@@ -1,21 +1,42 @@
 import requests
-url = "https://randomuser.me/api/?results=100"
 
-results = requests.get(url)
-i = 0
-for res in results.json()['results']:
-    i += 1
+total_results = 5
+fields = "name,gender,dob,email"
+
+# peticion get
+url = "https://randomuser.me/api/?results={results}&inc={fields}".format(
+    results=total_results,
+    fields=fields)
+results = requests.get(url).json()
+
+# se extraen los datos
+users = []
+for res in results['results']:
+
+    """
+        Almacenamos cada valor necesario del diccionario
+        en una variable.
+    """
     title = res['name']['title']
     first = res['name']['first']
     last = res['name']['last']
+    full_name = "{} {} {}".format(title, first, last)
+    age = res['dob']['age']
     gender = res['gender']
     email = res['email']
-    age = res['dob']['age']
-    print("{i} {gender} {title} {first} {last} {email} {age}".format(
-        i=i,
-        gender=gender,
-        title=title,
-        first=first,
-        last=last,
-        email=email,
-        age=age))
+    """
+        Para un mejor tratamiento generamos un nuevo
+        diccionario con los datos
+    """
+    users.append({"name": full_name,
+                  "age": age,
+                  "gender": gender,
+                  "email": email})
+    """
+        Ordena por edad descendente
+    """
+    users.sort(key=lambda x: x['age'], reverse=True)
+
+# se crea excel
+for user in users:
+    print("{} {} {}".format(user['gender'], user['name'], user['age']))
